0384a3cebf41acffc9bbab09789d5f503a6dc51a
Yoonsung <jeffkim1118@outlook.com>
2021-09-17 15:49:23 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function superbowlWin(record){
+    let x = 'W'
 
+    const found = record.find(({ result }) => result === x);
+    
+    //console.log(found.year);
+    
+    if (found == undefined){
+        return undefined;
+    } else {
+        return found.year;
+    }
+   
+}
